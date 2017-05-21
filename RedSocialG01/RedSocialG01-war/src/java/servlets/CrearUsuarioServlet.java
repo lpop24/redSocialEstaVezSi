8f2aee8f1566770b4e3b61f17765d1d12afcf889
Usuario <Usuario@DESKTOP-4A75LLF>
2017-05-21 21:20:29 +0200

    Cambios Loubna

--- RedSocialG01/RedSocialG01-war/src/java/servlets/CrearUsuarioServlet.java
+++ RedSocialG01/RedSocialG01-war/src/java/servlets/CrearUsuarioServlet.java
@@ -5,11 +5,18 @@
  */
 package servlets;
 
+import entity.Login;
+import entity.Usuario;
 import facade.EstudiosFacade;
+import facade.ExperiencialaboralFacade;
 import facade.LoginFacade;
 import facade.UsuarioFacade;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,6 +30,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "CrearUsuarioServlet", urlPatterns = {"/CrearUsuarioServlet"})
 public class CrearUsuarioServlet extends HttpServlet {
+
+    @EJB
+    private ExperiencialaboralFacade experiencialaboralFacade;
 
     @EJB
     private LoginFacade loginFacade;
@@ -45,7 +55,55 @@ public class CrearUsuarioServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ParseException {
+        
+        
+        /**
+         * LOGIN
+         **/
+        
+        Login login = new Login();
+        
+        String usuarioLogin = request.getParameter("usuarioLogin");
+        String contrasena = request.getParameter("contrasena");
+                
+        login.setUsuarioLogin(usuarioLogin);
+        login.setPassword(contrasena);
+        
+        this.loginFacade.create(login);
+        
+        
+        /**
+         * USUARIO
+         */
+        
+        Usuario usuario = new Usuario();
+        
+        String nombre = request.getParameter("nombre");
+        String apellidos = request.getParameter("apellidos");
+        java.util.Date fechaNacimiento = new SimpleDateFormat("dd-MM-yyy").parse(request.getParameter("fechaNacimiento"));
+        String instagram = request.getParameter("instagram");
+        String correoElectronico = request.getParameter("correoElectronico");
+        String twitter = request.getParameter("twitter");
+        String telefono = request.getParameter("telefono");
+        String paginaWebUsuario = request.getParameter("paginaWebUsuario");
+        String aficiones = request.getParameter("aficiones");
+        String ciudad = request.getParameter("ciudad");
+        
+        usuario.setNombre(nombre);
+        usuario.setApellidos(apellidos);
+        usuario.setFechaNacimiento(fechaNacimiento);
+        usuario.setInstagram(instagram);
+        usuario.setCorreoElectronico(correoElectronico);
+        usuario.setTwitter(twitter);
+        usuario.setTelefono(telefono);
+        usuario.setPaginaWebUsuario(paginaWebUsuario);
+        usuario.setAficiones(aficiones);
+        usuario.setCiudad(ciudad);
+        usuario.setLoginFK(login);
+        
+        this.usuarioFacade.create(usuario);
+        
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -73,7 +131,11 @@ public class CrearUsuarioServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(CrearUsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -87,7 +149,11 @@ public class CrearUsuarioServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ParseException ex) {
+            Logger.getLogger(CrearUsuarioServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- RedSocialG01/RedSocialG01-war/web/crearUsuario.jsp
+++ RedSocialG01/RedSocialG01-war/web/crearUsuario.jsp
@@ -4,14 +4,80 @@
     Author     : Loubna Channouf Cherradi
 --%>
 
+<%@page import="entity.Login" %>
+<%
+    Login login = new Login();
+%>
+
+<%@page import="entity.Usuario" %>
+<%
+    Usuario usuario = new Usuario();
+%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Nuevo Usuario</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Crear Perfil</h1>
+        <form method="post" action="CrearUsuarioServlet">
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td>Nombre de Usuario </td>
+                        <td><input type="text" name="usuarioLogin" /></td>
+                    </tr>
+                    <tr>
+                        <td>Contraseña</td>
+                        <td><input type="password" name="contrasena" /></td>
+                    </tr>
+                    <tr>
+                        <td>Nombre:</td>
+                        <td><input type="text" name="nombre" ></td>
+                    </tr>
+                    <tr>
+                        <td>Apellidos:</td>
+                        <td><input type="text" name="apellidos" ></td>
+                    </tr>
+                    <tr>
+                        <td>Instagram:</td>
+                        <td><input type="text" name="instagram" ></td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td><input type="text" name="correoElectronico" ></td>
+                    </tr>
+                    <tr>
+                        <td>Twitter:</td>
+                        <td><input type="text" name="twitter" ></td>
+                    </tr>
+                    <tr>
+                        <td>Telefono:</td>
+                        <td><input type="text" name="telefono" ></td>
+                    </tr>
+                    <tr>
+                        <td>Fecha de nacimiento:</td>
+                        <td><input type="date" name="fechaNacimiento"  ></td>
+                    </tr>
+                    <tr>
+                        <td>Pagina web:</td>
+                        <td><input type="text" name="pagina_web" ></td>
+                    </tr>
+                    <tr>
+                        <td>Aficiones:</td>
+                        <td><input type="text" name="aficiones" ></td>
+                    </tr>
+                    <tr>
+                        <td>Ciudad:</td>
+                        <td><input type="text" name="ciudad" ></td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+                    
+            <button>Guardar</button>
+        </form>
     </body>
 </html>

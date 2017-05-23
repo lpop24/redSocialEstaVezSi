9a3942f6a37554afa1d5bc72c7038e45ef60b298
Usuario <Usuario@DESKTOP-4A75LLF>
2017-05-23 17:08:32 +0200

    Qué agobio

--- RedSocialG01/RedSocialG01-war/src/java/servlets/CrearUsuarioServlet.java
+++ RedSocialG01/RedSocialG01-war/src/java/servlets/CrearUsuarioServlet.java
@@ -67,6 +67,7 @@ public class CrearUsuarioServlet extends HttpServlet {
          * LOGIN
          **/
         
+        
         Login login = new Login();
         
         String usuarioLogin = request.getParameter("usuarioLogin");
